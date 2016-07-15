03d28f41f83fd6ecd19650cf7c0b91a8bdc3613a
Jonas Großmann <jonas.github@naevius.de>
2016-07-15 13:10:53 +0200

    Added different game states

--- src/Objects.elm
+++ src/Objects.elm
@@ -1,14 +1,19 @@
-module Objects exposing (foo)
+module Objects exposing (..)
 
 import Collage exposing (..)
 import Color exposing (..)
-import Time exposing (millisecond)
+import Time exposing (millisecond, Time)
+
 
 type alias Object =
-    { pos : (Float, Float), ttl : Time, form : Form }
+    { pos : ( Float, Float ), ttl : Time, form : Form }
+
 
 foo : Form
-foo = scale 3 (filled black (polygon [(0,0), (10,-10), (0,-10), (-10,0)]))
+foo =
+    scale 3 (filled black (polygon [ ( 0, 0 ), ( 10, -10 ), ( 0, -10 ), ( -10, 0 ) ]))
 
-make_object : (Float, Float) -> Form -> Object
-make_object ((x, y) as pos) f = Object pos (6000 * millisecond) f
+
+makeObject : ( Float, Float ) -> Form -> Object
+makeObject (( x, y ) as pos) f =
+    Object pos (6000 * millisecond) f
